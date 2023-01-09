b58bc9791aae12b5549347bbe09614485d39d290
Jacson Passold <jacson@passold.com.br>
2023-01-09 15:54:06 -0300

    add divi compatibility

--- class/RLDateFormat.php
+++ class/RLDateFormat.php
@@ -10,8 +10,9 @@ class RLDateFormat {
         }
     }
     public function init() {
+    	$this->load_textdomain();
         add_filter( 'get_the_time',  array($this, 'post_time_ago' ), 10, 3 );
-        $this->load_textdomain();
+        add_filter( 'date_i18n',  array($this, 'date_i18n' ), 10, 4 );
     }
         
     public function load_textdomain() {
@@ -21,8 +22,29 @@ class RLDateFormat {
     /**
      * Based on https://wpklik.com/wordpress-tutorials/display-time-ago/
      */
-    function post_time_ago($the_time, $format, $post) {
-    	return sprintf( esc_html__( '%s ago', 'rl-date-format' ), human_time_diff(get_post_time('U', false, $post, true), current_time( 'timestamp' ) ) );
+    public function post_time_ago($the_time, $format, $post) {
+    	return $this->format_date($the_time, $post);
+    }
+    
+    public function format_date($date, $post = null) {
+    	if(isset($post)) {
+    		return sprintf( esc_html__( '%s ago', 'rl-date-format' ), human_time_diff(get_post_time('U', false, $post, true), current_time( 'timestamp' ) ) );
+    	} elseif(is_int($date)) {
+    		return sprintf( esc_html__( '%s ago', 'rl-date-format' ), human_time_diff($date, current_time( 'timestamp' ) ) );
+    	}
+    	return $date;
+    }
+    
+    /**
+     * 
+     * @param string $j          Formatted date string.
+	 * @param string $req_format Format to display the date.
+	 * @param int    $i          Unix timestamp.
+	 * @param bool   $gmt        Whether to convert to GMT for time. Default false.
+     */
+    public function date_i18n($j, $req_format, $i, $gmt) {
+    	
+    	return $this->format_date($i);
     }
 }
 
